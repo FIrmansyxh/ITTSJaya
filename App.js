49596e14f6eeb2479fad587c9f793d467fef367b
FIrmansyxh <118160076+FIrmansyxh@users.noreply.github.com>
2022-11-19 20:45:16 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import {StyleSheet,TouchableOpacity,Text,View,} from 'react-native'
+
+class App extends Component {
+  state = {
+    namaku: "Firmansyah"
+  }
+
+  change = () => {
+    this.setState({
+      namaku: "Muhammad Firmansyah"
+    })
+  }
+
+ render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+         style={styles.button}
+         onPress={this.change}
+        >
+         <Text style={{color: 'white',
+          fontSize: 20}}> klik untuk nama Lengkap</Text>
+        </TouchableOpacity>
+        <View>
+          <Text>
+            {this.state.namaku}
+          </Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#00008B',
+    padding: 20,
+    marginBottom: 20
+  }
+})
+
+export default App;
